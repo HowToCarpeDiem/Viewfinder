cfd9221e52a55b25aa4c1a28704618e3665fa57a
HowToCarpeDiem <kamilburza03@gmail.com>
2026-05-19 22:28:53 +0200

    'f/tools_panel'

--- viewfinder.py
+++ viewfinder.py
@@ -1,6 +1,6 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QFileDialog
-from PySide6.QtGui import QImage, QPixmap, QAction
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QFileDialog, QStackedWidget
+from PySide6.QtGui import QImage, QPixmap, QAction, QIcon
 from PySide6.QtCore import Qt
 import cv2
 
@@ -22,6 +22,29 @@ class MainWindow(QMainWindow):
         #Lewy panel
         self.ly_left = QVBoxLayout()
         self.ly_main.addLayout(self.ly_left)
+
+        #Górne okno, lewego panelu - pasek narzędzi
+        self.ly_tools = QHBoxLayout()
+        self.ly_left.addLayout(self.ly_tools)
+
+        #Dolne okno, lewego panelu - ustawienia narzędzi
+        self.tool_options = QStackedWidget()
+        self.ly_left.addWidget(self.tool_options)
+
+        #Pusty lewy panel
+        self.tools_options_default = QWidget()
+        ly_tools_options_default = QVBoxLayout()
+
+        self.tools_options_default.setLayout(ly_tools_options_default)
+        ly_tools_options_default.addWidget(QLabel('Choose tool'))
+        self.tool_options.addWidget(self.tools_options_default)
+
+        #Panel dla grayscale
+        self.grayscale = QWidget()
+        ly_grayscale = QVBoxLayout()
+
+        self.grayscale.setLayout(ly_grayscale)
+        self.tool_options.addWidget(self.grayscale)
 
         #Prawy panel
         self.ly_right = QVBoxLayout()
@@ -45,14 +68,15 @@ class MainWindow(QMainWindow):
         self.lb_image = QLabel('Image')
         self.ly_right.addWidget(self.lb_image)
 
-        #Przycisk wczytywania obrazu
-        # self.btn_load = QPushButton('Load image')
-        # self.ly_left.addWidget(self.btn_load)
-        # self.btn_load.clicked.connect(self.load_image)
-
+        #Pasek narzędzi
+        self.btn_tools1 = QPushButton()
+        self.btn_tools1.setIcon(QIcon('assets/grayscale.png'))
+        self.ly_tools.addWidget(self.btn_tools1)
+        self.btn_tools1.clicked.connect(self.open_tools1_panel)
+        
         #Skala szarości
         self.btn_grayscale = QPushButton('Grayscale')
-        self.ly_left.addWidget(self.btn_grayscale)
+        ly_grayscale.addWidget(self.btn_grayscale)
         self.btn_grayscale.clicked.connect(self.apply_grayscale)
 
 
@@ -81,6 +105,9 @@ class MainWindow(QMainWindow):
             img_scaled = img.scaled(800, 600, Qt.KeepAspectRatio, Qt.SmoothTransformation)
             self.lb_image.setPixmap(img_scaled)
             self.img_current = img_gray
+
+    def open_tools1_panel(self):
+        self.tool_options.setCurrentWidget(self.grayscale)
 
 
     def save_image(self):
